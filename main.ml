6dc266b5a4029b2faf64ffbd70b38465b196e692
Dimitri Medeiros <79457494+itznokx@users.noreply.github.com>
2023-11-20 13:58:09 -0300

    Update main.ml
    Include type nat boolean and term (to do). Organizing functions

--- main.ml
+++ main.ml
@@ -1,19 +1,34 @@
+(*-Typing variants-*)
+type nat 		= Nat of int ;;
+
+type boolean	= Bool of bool;;
+
+type term       = 	| TT_Nat of nat 
+					| TT_Bool of bool 
+					| Suc of nat
+					| Pred of nat
+					| EhZero of nat
+;;
+
+
+
+
+
+(*-Program code-*)
 let rec show 	(l1: string list) (count: int) =
 	match l1 with
 	| h::t 	-> Printf.printf "[%d] -> %s\n"count h; show t (count+1)
 	| []	-> ()
 ;;
 
-
-
 let separate 	(expression:string) : string list =
 	let l1 = String.split_on_char ' ' expression in
 	show l1 0; l1;;
 
-let rec analyze		(l1 : string list) 	=
 	
 
 let programa () =
-    separate (read_line ());;
+    let readline = read_line () in
+    separate (readline) ;;
 
 programa ();;
