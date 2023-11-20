b5eecd63dc4409484df038c9f91e5252e0625f72
Dimitri Medeiros <79457494+itznokx@users.noreply.github.com>
2023-11-20 15:42:39 -0300

    Update main.ml

--- main.ml
+++ main.ml
@@ -3,15 +3,12 @@ type nat 		= 	| NAT 			of int ;;
 
 type boolean	= 	| BOOL 			of bool ;;
 
-type nat->nat   =   | SUC 			of nat 
-					| PRED 			of nat
-
 type term       = 	| TERM_NAT 		of nat 
 					| TERM_BOOL 	of bool 
-					| TERM_SUC 		of nat
-					| TERM_PRED 	of nat
-					| TERM_EHZERO	of nat
-;;
+					| TERM_SUC 		of string
+					| TERM_PRED 	of string
+					| TERM_EHZERO	of string
+					;;
 
 
 
@@ -23,6 +20,14 @@ let rec show 	(l1: string list) (count: int) =
 	| h::t 	-> Printf.printf "[%d] -> %s\n"count h; show t (count+1)
 	| []	-> ()
 ;;
+
+let analyze (exp: 'a) : term =
+	match exp with
+	| "true" 	-> TERM_BOOL 	true
+	| "false" 	-> TERM_BOOL 	false
+	| "suc"		-> TERM_SUC 	"suc"
+	| "pred"	-> TERM_PRED 	"pred"
+	| "ehzero"  -> TERM_EHZERO  "ehzero"
 
 let separate 	(expression:string) : string list =
 	let l1 = String.split_on_char ' ' expression in
